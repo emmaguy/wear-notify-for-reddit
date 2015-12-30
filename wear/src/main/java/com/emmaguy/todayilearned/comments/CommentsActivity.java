71a9219ebfff81779af42f0cc82be45c24109219
Emma Guy <emmag87@gmail.com>
2015-12-30 16:02:54 +0000

    Tidy up, use an else rather than a return

--- wear/src/main/java/com/emmaguy/todayilearned/comments/CommentsActivity.java
+++ wear/src/main/java/com/emmaguy/todayilearned/comments/CommentsActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.view.DotsPageIndicator;
 import android.support.wearable.view.GridViewPager;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowInsets;
 import android.widget.Toast;
@@ -28,33 +29,32 @@ public class CommentsActivity extends Activity implements ActionFragment.OnActio
 
         setContentView(R.layout.activity_comments);
 
-        String stringComments = getIntent().getStringExtra(Constants.KEY_REDDIT_POSTS);
+        final String stringComments = getIntent().getStringExtra(Constants.KEY_REDDIT_POSTS);
         final ArrayList<Comment> comments = mGson.fromJson(stringComments, new TypeToken<List<Comment>>() {}.getType());
         if (comments == null || comments.isEmpty()) {
             Toast.makeText(this, R.string.thread_has_no_comments_yet, Toast.LENGTH_SHORT).show();
             finish();
-            return;
+        } else {
+            mGridViewPager = (GridViewPager) findViewById(R.id.pager);
+            mGridViewPager.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
+                @Override
+                public WindowInsets onApplyWindowInsets(View v, WindowInsets insets) {
+                    // A little extra horizontal spacing between pages looks a bit less crowded on a round display
+                    int rowMargin = getResources().getDimensionPixelOffset(R.dimen.page_row_margin);
+                    int colMargin = getResources().getDimensionPixelOffset(insets.isRound() ? R.dimen.page_column_margin_round : R.dimen.page_column_margin);
+                    mGridViewPager.setPageMargins(rowMargin, colMargin);
+
+                    // GridViewPager relies on insets to properly handle layout for round displays
+                    // They must be explicitly applied since this listener has taken them over
+                    mGridViewPager.onApplyWindowInsets(insets);
+                    return insets;
+                }
+            });
+            mGridViewPager.setAdapter(new CommentsGridPagerAdapter(CommentsActivity.this, getFragmentManager(), comments));
+
+            DotsPageIndicator dotsPageIndicator = (DotsPageIndicator) findViewById(R.id.page_indicator);
+            dotsPageIndicator.setPager(mGridViewPager);
         }
-
-        mGridViewPager = (GridViewPager) findViewById(R.id.pager);
-        mGridViewPager.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
-            @Override
-            public WindowInsets onApplyWindowInsets(View v, WindowInsets insets) {
-                // A little extra horizontal spacing between pages looks a bit less crowded on a round display
-                int rowMargin = getResources().getDimensionPixelOffset(R.dimen.page_row_margin);
-                int colMargin = getResources().getDimensionPixelOffset(insets.isRound() ? R.dimen.page_column_margin_round : R.dimen.page_column_margin);
-                mGridViewPager.setPageMargins(rowMargin, colMargin);
-
-                // GridViewPager relies on insets to properly handle layout for round displays
-                // They must be explicitly applied since this listener has taken them over
-                mGridViewPager.onApplyWindowInsets(insets);
-                return insets;
-            }
-        });
-        mGridViewPager.setAdapter(new CommentsGridPagerAdapter(CommentsActivity.this, getFragmentManager(), comments));
-
-        DotsPageIndicator dotsPageIndicator = (DotsPageIndicator) findViewById(R.id.page_indicator);
-        dotsPageIndicator.setPager(mGridViewPager);
     }
 
     @Override
